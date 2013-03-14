f4b7bbd9c11c1b8bb1fc98bec43abd9ca94df2c5
mattgarrish <matt.garrish@gmail.com>
2013-03-14 13:57:30 -0400

    adding batch-equiv build script

---
+++ util/java/build.bat
@@ -0,0 +1,10 @@
+@ECHO OFF
+SET JARS=lib/*;lib/epubcheck-3.0-RC-2/*
+ECHO "compiling..."
+javac -d bin -cp %JARS% -sourcepath src src/org/idpf/epub/testsuite/FormBuilder.java
+javac -d bin -cp %JARS% -sourcepath src src/org/idpf/epub/testsuite/ZipBuilder.java
+ECHO "building results form..."
+java -cp %JARS%;bin org.idpf.epub.testsuite.FormBuilder
+ECHO "building epubs..."
+java -cp %JARS%;bin org.idpf.epub.testsuite.ZipBuilder
+ECHO "... done."
