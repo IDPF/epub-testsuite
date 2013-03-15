2a2359972ff6cf78b42e90d55017f0e07c7b3f98
mattgarrish <matt.garrish@gmail.com>
2013-03-14 21:15:32 -0400

    patching batch file to add bin dir if it doesn't exist

--- util/java/build.bat
+++ util/java/build.bat
@@ -1,5 +1,6 @@
 @ECHO OFF
 SET JARS=lib/*;lib/epubcheck-3.0-RC-2/*
+IF NOT EXIST bin mkdir bin
 ECHO "compiling..."
 javac -d bin -cp %JARS% -sourcepath src src/org/idpf/epub/testsuite/FormBuilder.java
 javac -d bin -cp %JARS% -sourcepath src src/org/idpf/epub/testsuite/ZipBuilder.java
