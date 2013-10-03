c1ad558ff805cc89250f6f8bf00401157075dd32
Daniel Weck <daniel.weck@gmail.com>
2013-10-03 22:16:43 +0100

    recursively delete .DS_Store invisible MacOSX files from build.sh script + fixed a few erroneous SMIL epub:textrefs

--- util/java/build.sh
+++ util/java/build.sh
@@ -10,6 +10,17 @@ javac -d bin -cp $JARS -sourcepath src src/org/idpf/epub/testsuite/ZipBuilder.ja
 echo "building results form..."
 java -cp $JARS:bin org.idpf.epub.testsuite.FormBuilder
 
+
+root=$(pwd)
+EPUB_FOLDER="${root}/../../content/30/"
+echo "${EPUB_FOLDER}"
+find "${EPUB_FOLDER}" -name ".DS_Store" -depth -exec rm {} \;
+#for x in `find ./$@ -name ".DS_Store" -print`
+#   do
+#     rm -f $x
+#   done
+
+
 echo "building epubs..."
 java -cp $JARS:bin org.idpf.epub.testsuite.ZipBuilder
 
